7e11cd9c2e2d546c2a10dda5d5e1025e1dbf361d
Patrick <pcrussel@ucsc.edu>
2016-06-23 14:00:57 -0700

    Added Obstacle

---
+++ Assets/Controler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Controler : MonoBehaviour {
+	public float landing_tolerance = 100;
+
+	private Rigidbody2D rb;
+	// Use this for initialization
+	void Start () {
+		rb = transform.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter (Collision col){
+		
+	}
+}
---
+++ Assets/Scripts/Player/Player_Status.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_Status : MonoBehaviour {
+
+		public float landing_tolerance = 100;
+
+		private Rigidbody2D rb;
+		// Use this for initialization
+		void Start () {
+			rb = transform.GetComponent<Rigidbody2D>();
+		}
+
+		// Update is called once per frame
+		void Update () {
+
+		}
+
+		void OnCollisionEnter (Collision col){
+
+		}
+
+
+}
