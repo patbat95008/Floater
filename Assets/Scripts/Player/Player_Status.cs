631b3cb2ca887cacd06b35bbee69349063565629
Patrick <pcrussel@ucsc.edu>
2016-06-23 14:54:43 -0700

    Added Crash Detection
    The ship now knows if it's going in too fast for a landing.

--- Assets/Scripts/Player/Player_Status.cs
+++ Assets/Scripts/Player/Player_Status.cs
@@ -2,23 +2,52 @@
 using System.Collections;
 
 public class Player_Status : MonoBehaviour {
+	public int playerHealth = 100;
+	public float landing_tolerance = .75f;
+	public bool touchdown = false;
+	public bool crashed = false;
+	public int impact_hurt = 30;
 
-		public float landing_tolerance = 100;
+	private Rigidbody2D rb;
+	private float z_speed;
+	// Use this for initialization
+	void Start () {
+		rb = transform.GetComponent<Rigidbody2D>();
+	}
 
-		private Rigidbody2D rb;
-		// Use this for initialization
-		void Start () {
-			rb = transform.GetComponent<Rigidbody2D>();
+	// Update is called once per frame
+	void Update () {
+		z_speed = rb.velocity.y;
+		Debug.Log("Downward Speed = " + z_speed);
+	}
+
+
+
+	void OnCollisionEnter2D(Collision2D col){
+		Collider2D collider = col.collider;
+		Vector3 contactPoint = col.contacts[0].point;
+		Vector3 center = collider.bounds.center;
+		float colWidth = collider.bounds.size.x;
+		float colHeight = collider.bounds.size.y;
+		//If the collision is the side, hurt the player1
+		bool hitRight = contactPoint.x >= (center.x + colWidth/2);
+		bool hitLeft = contactPoint.x <= (center.x - colWidth/2);
+
+		if(hitRight || hitLeft){
+			playerHealth -= impact_hurt;
 		}
 
-		// Update is called once per frame
-		void Update () {
+		//If it's from the top, check the landing
+		bool hitTop = (contactPoint.y >= (center.y + colHeight/2) )
+			&& (contactPoint.x <= (center.x + colWidth/2) )
+			&& (contactPoint.x >= (center.x - colWidth/2) );
 
+		if(hitTop && Mathf.Abs(z_speed) > landing_tolerance){ //crashed
+			crashed = true;
+			touchdown = false;
+		}else if(hitTop && Mathf.Abs(z_speed) < landing_tolerance){ //landed
+			crashed = false;
+			touchdown = true;
 		}
-
-		void OnCollisionEnter (Collision col){
-
-		}
-
-
+	}
 }
