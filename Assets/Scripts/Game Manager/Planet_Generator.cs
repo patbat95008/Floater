94455cc9a325bc814d2cbdc9eccb105a464766cb
Patrick <pcrussel@ucsc.edu>
2016-06-29 17:19:58 -0700

    Initial Generator Fields Added

--- Assets/Scripts/Game Manager/Planet_Generator.cs
+++ Assets/Scripts/Game Manager/Planet_Generator.cs
@@ -2,10 +2,16 @@
 using System.Collections;
 
 public class Planet_Generator : MonoBehaviour {
+	public GameObject[] floaters, grounds, cliffs, walls, treasures;
+	public int floater_density, hole_density, hole_size_y, hole_size_x;
 
+	private float managerPOS;
 	// Use this for initialization
 	void Start () {
-	
+		//Create floating astroids randomly according to density
+		//Create level ground
+		//Carve holes into the ground of size x and y
+		//Add treasures
 	}
 	
 	// Update is called once per frame
