dc83de4aa270234cc1d1b1a4c77b0c990bdb7c0a
Patrick <pcrussel@ucsc.edu>
2016-07-14 16:32:17 -0700

    Split Planet Generator
    The Floater generator should really be its own thing.

---
+++ Assets/Scripts/Game Manager/Floater_Gen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Floater_Gen : MonoBehaviour {
+	public GameObject[] floaters;
+	public int floater_density;
+
+	// Use this for initialization
+	void Start () {
+		//Create floating astroids randomly according to density
+		for(int i = 0; i < floater_density; i++){
+			int rand = (int)Random.Range(0,floaters.Length);
+			Instantiate(floaters[rand], 
+				new Vector3(Random.Range(-25f, 25f), Random.Range(-25f, 25f), 0),
+				new Quaternion());
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/Game Manager/Planet_Generator.cs
+++ Assets/Scripts/Game Manager/Planet_Generator.cs
@@ -2,20 +2,13 @@
 using System.Collections;
 
 public class Planet_Generator : MonoBehaviour {
-	public GameObject[] floaters, grounds, cliffs, treasures;
+	public GameObject[] grounds, cliffs, treasures;
 	public int floater_density, space_distance, hole_density;
 
 	private float managerPOS;
 	private GameObject[] ground_game;
 	// Use this for initialization
 	void Start () {
-		//Create floating astroids randomly according to density
-		for(int i = 0; i < floater_density; i++){
-			int rand = (int)Random.Range(0,floaters.Length);
-			Instantiate(floaters[rand], 
-				new Vector3(Random.Range(-25f, 25f), Random.Range(-25f, 25f), 0),
-				new Quaternion());
-		}
 		//Create level ground
 		for(int i = -50; i < 50; i+= 2){
 			int rand = (int)Random.Range(0,grounds.Length);
